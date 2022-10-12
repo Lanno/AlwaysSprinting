9063a3fefc3315b1afa645af8a2647a8468ad67b
Nolan <noluck589@gmail.com>
2022-10-11 19:30:18 -0700

    made velocity changes depend on avatar mass

--- MelonLoaderMod.cs
+++ MelonLoaderMod.cs
@@ -2,6 +2,7 @@
 using SLZ.Rig;
 using UnityEngine;
 using System.IO;
+using SLZ.VRMK;
 
 namespace AlwaysSprinting
 {
@@ -16,39 +17,59 @@ namespace AlwaysSprinting
 
     public class AlwaysSprinting : MelonMod
     {
-        ControllerRig rig;
-        float defaultMaxVelocity;
+        private ControllerRig rig;
+        private PhysicsRig physicsRig;
+        private PhysTorso physTorso;
+
+        private float defaultMaxVelocity;
+        private float previousMass;
+        private bool waitForVelocityChange;
+
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             rig = Object.FindObjectOfType<ControllerRig>();
-
-            if (rig)
-            {
-                defaultMaxVelocity = rig.maxVelocity;
-            }
+            physicsRig = Object.FindObjectOfType<PhysicsRig>();
+            physTorso = Object.FindObjectOfType<PhysTorso>();
         }
+
+        public float GetMass()
+        {
+            float mass = 0;
+
+            if (physTorso)
+                mass = physTorso.rbChest.mass + physTorso.rbHead.mass + physTorso.rbPelvis.mass + physTorso.rbSpine.mass;
+
+            return mass;
+        }
+
         public override void OnUpdate()
         {
-            if (!rig)
-            {
+            if (!rig || !physTorso)
                 return;
-            }
 
+            // Disable double tap sprint
             if (rig._wasOverFlickThresh)
-            {
                 rig._wasOverFlickThresh = false;
+
+            // Use change in mass to check if avatar has changed. 
+            if (1.05 * previousMass <= GetMass() || GetMass() <= 0.95 * previousMass)
+            {
+                previousMass = GetMass();
+                waitForVelocityChange = true;
             }
 
-            // Attempt to determine if max velocity has changed based on avatar swap
-            if ((-0.01 < rig.jumpVelocity && rig.jumpVelocity < 0.01) && (2.1f * defaultMaxVelocity <= rig.maxVelocity || rig.maxVelocity <= 1.9f * defaultMaxVelocity))
+            // Check flag and wait for an unscaled velocity value to appear.
+            if (waitForVelocityChange && (rig.maxVelocity < 1.9f * defaultMaxVelocity || defaultMaxVelocity == 0))
             {
                 defaultMaxVelocity = rig.maxVelocity;
+                waitForVelocityChange = false;
             }
 
-            // Guard against multiple entries
-            if (rig.maxVelocity < defaultMaxVelocity * 1.9f)
+            // Only change when flagged and when not moving vertically. The latter is to prevent errors if the change occurs 
+            // during freefall. The engine changes rig.maxVelocity during freefall to represent critical velocity.
+            if (!waitForVelocityChange && (-0.1 < physicsRig.wholeBodyVelocity.y && physicsRig.wholeBodyVelocity.y < 0.1))
             {
-                rig.maxVelocity = defaultMaxVelocity * 2.0f;
+                rig.maxVelocity = 2.0f * defaultMaxVelocity;
             }
         }
     }
